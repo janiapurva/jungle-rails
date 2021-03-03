58dd76020f0cbda0c9e761f58ed46cf3abba6787
janiapurva <jani.apurva@yahoo.com>
2021-03-03 16:19:17 -0500

    implemnt dashboard feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
+  
 
   def show
+  @product_count = Product.count
+  @category_list = Product.group(:category_id).count
   end
 end
