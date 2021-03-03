3a7a1b8d9260efe594538630c475c4c840e015ae
janiapurva <jani.apurva@yahoo.com>
2021-03-03 17:05:13 -0500

    add about us routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  get '/about', to: 'about#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -15,7 +16,12 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+
   end
+  
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
